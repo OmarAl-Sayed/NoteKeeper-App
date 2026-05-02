b04c5344f6796672d98fdfa9dedabc0aff9081e7
omar <omarelsayed.oes@gmail.com>
2026-05-02 23:45:48 +0300

    refactor: replace Body component with dynamic Note rendering; add notes data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,18 @@
-import Body from "./Body"
 import Footer from "./Footer"
 import Heading from "./Heading"
+import notes from "./notes.js"
+import Note from "./Note.jsx"
 
+let x="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, temporibus? Veniam, fuga sed nihil delectus iste eaque magni eveniet, ex sint explicabo perferendis velit. Temporibus fugiat odit qui delectus? Maiores consequuntur unde dolores soluta illum hic quidem sunt nesciunt culpa. Iste temporibus dicta vel asperiores eum odio nihil, magnam reiciendis? Totam quam vitae quia provident earum quod dolorum. Placeat dolor maxime qui nisi optio enim illum quasi debitis veniam autem ea impedit provident voluptas, soluta eius sunt similique sequi distinctio repellendus quia. Eum deleniti nesciunt vel nihil error repellendus recusandae hic placeat atque qui corporis ea architecto, saepe quas sunt?"
 
+console.log(notes);
 
 
 
 function App() {
     return (<>
         <Heading />
-        <Body/>
+        {notes.map(nt => <Note key={nt.key} title={nt.title} content={nt.content} />)}
         <Footer/>
         </>
 )
